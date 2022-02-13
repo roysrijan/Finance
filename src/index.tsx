1d98e7ed057cac16213988c950d29521eada146e
roysrijan <roy.srijan@outlook.com>
2022-02-13 22:35:50 +0530

    API Key Updated

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 import { MoralisProvider } from "react-moralis";
 
 ReactDOM.render(
-    <MoralisProvider appId="RU05ViCe03YWwSaIidoSpK2wkaI1eAT9E0S7mayJ" serverUrl="https://odd0lkeziyd3.usemoralis.com:2053/server">
+    <MoralisProvider appId="gKECafvmKC1HZerUI2xlpqYcDENCozuKvTJRTIlM" serverUrl="https://nbsbir88n3ng.usemoralis.com:2053/server">
           <App />
     </MoralisProvider>,
   document.getElementById('root')
